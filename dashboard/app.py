6d23b7c8948d0c2e9c4d29ab8599305a60d7f6fc
ndeye022 <136347971+ndeye022@users.noreply.github.com>
2026-04-22 13:51:39 +0200

    Remove Streamlit app content from app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,1 @@
-import streamlit as st
-st.title("🔬 Plateforme IA - LRGP Nancy")
-st.write("Bienvenue sur la plateforme de deploiement des modèles IA du laboratoire")
 
-st.markdown("##Aplication disponible ")
-st.link_button("Modèle Thermodynamique", "http://localhost:8501")
-st.info("D'autres modèles pouront etre ajoutés prochainement." )
